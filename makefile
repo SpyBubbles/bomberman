d7d3359a3c407aec9957b94aede1a378ee149fe6
SpyBubbles <160052026+SpyBubbles@users.noreply.github.com>
2024-04-19 01:52:29 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 bin/tui: src/tui_test.cpp
-	g++ src/tui_test.cpp -o bin/tui -I include -l ftxui-screen -l ftxui-dom -l ftxui-component
+	g++ src/tui_test.cpp -o bin/tui -I include -l ftxui-screen -l ftxui-dom -l ftxui-component -std=c++2a
 
 run: bin/tui
 	./bin/tui
--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,26 +1,38 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    const std::string texto = "Hola Mundo";
-    Element textElement = text(texto);
-    
-    Element dibujo = hbox(textElement | border);
+    int fotograma = 0;
+    string reset;
+
+    while (true){
+    fotograma++;
+    Element personaje = spinner(17,fotograma);
+    Decorator colorFondo = bgcolor(Color::DarkBlue);
+    Decorator colorTexto = color(Color::Yellow);
+    Element dibujo = border ({
+        hbox(personaje) | colorFondo | colorTexto 
+    });
 
     Dimensions Alto = Dimension::Fixed(10);
-    Dimensions Ancho = Dimension::Fixed(10);
+    Dimensions Ancho = Dimension::Full();
 
     Screen pantalla = Screen::Create(Ancho,Alto);
 
     Render(pantalla,dibujo);
     pantalla.Print();
-    cout << endl;
-
+    reset = pantalla.ResetPosition();
+    cout << reset;
+    this_thread::sleep_for(0.1s);
+    }
+    
     return 0;
 }
