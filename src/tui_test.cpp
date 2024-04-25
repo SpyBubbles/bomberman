e830ab24fa746464c242bb2550e28ac74415bee6
SpyBubbles <160052026+SpyBubbles@users.noreply.github.com>
2024-04-25 00:58:18 +0000

    Version 3

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -4,28 +4,57 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <list>
+#include <fstream>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+    list<string> textos;
+    fstream imagen;
+    imagen.open("./assets/imagen.txt");
+
+    string linea;
+    while (getline(imagen,linea))
+    {
+        textos.push_back(linea);
+    }
+    imagen.close();
+
     int fotograma = 0;
     string reset;
+    int posX = 0;
+    int posY = 6;
 
     while (true){
     fotograma++;
-    Element personaje = spinner(17,fotograma);
+    Element personaje = spinner(21,fotograma);
     Decorator colorFondo = bgcolor(Color::DarkBlue);
     Decorator colorTexto = color(Color::Yellow);
     Element dibujo = border ({
         hbox(personaje) | colorFondo | colorTexto 
     });
 
-    Dimensions Alto = Dimension::Fixed(10);
+    Dimensions Alto = Dimension::Full();
     Dimensions Ancho = Dimension::Full();
 
     Screen pantalla = Screen::Create(Ancho,Alto);
+    Render (pantalla,dibujo);
+
+    int l = 0;
+    for (auto &&texto : textos)
+    {
+        int i = 0;
+        for (auto &&letra : texto)
+        {
+            pantalla.PixelAt(posX + i, posY + l).character = letra;
+            i++;
+        }
+        l++;
+    }
+    posX++;
 
     Render(pantalla,dibujo);
     pantalla.Print();
